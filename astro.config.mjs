c164aa981332ef8567b4ad66c6e6872bbaed44ea
Chaiyapong_Japan <japanouan88@gmail.com>
2026-05-29 16:22:53 +0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,7 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+  site: 'https://japanouan.github.io', 
+  base: '/Portfolio/', 
 });
